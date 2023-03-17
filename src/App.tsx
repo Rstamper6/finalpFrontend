0df01132935bbf4ff4dc6efb69d8b2e5f17e267b
Veronika Jones <veronikashapiro@yahoo.com>
2023-03-16 21:31:58 -0400

    add uploader

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import UploadWidget from "./components/UploadWidget";
 import "./App.css";
-import { Header } from './components/header';
+import { Header } from "./components/header";
 import { LandingPage } from "./components/landingPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import BoardsRoute from './routes/BoardsRoute';
+import BoardsRoute from "./routes/BoardsRoute";
 import { BoardPostsRoute } from "./routes/BoardPostsRoute";
 import BoardContextProvider from "./context/BoardContectProvider";
 
 function App() {
   return (
     <BoardContextProvider>
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/boards' element={<BoardsRoute />} />
-          <Route path='/boards/:id' element={<BoardPostsRoute />} />
-        </Routes>
-      </div>
-      {/* <UploadWidget /> */}
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/boards" element={<BoardsRoute />} />
+            <Route path="/boards/:id" element={<BoardPostsRoute />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </BoardContextProvider>
-
   );
 }
 
--- src/routes/BoardsRoute.tsx
+++ src/routes/BoardsRoute.tsx
@@ -1,20 +1,19 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 import { BoardsList } from "../components/BoardsList";
-import Board from '../models/GraveBook';
-import { fetchBoards } from '../services/gravebookServices';
-import { BoardForm } from '../components/addBoardForm';
-import BoardContext from '../context/BoardContext';
+import Board from "../models/GraveBook";
+import { fetchBoards } from "../services/gravebookServices";
+import { BoardForm } from "../components/addBoardForm";
+import BoardContext from "../context/BoardContext";
 
-export default function BoardsRoute () {
-  const {getBoards, boards} = useContext(BoardContext)
+export default function BoardsRoute() {
+  const { getBoards, boards } = useContext(BoardContext);
 
-  useEffect(getBoards, [])
+  useEffect(getBoards, []);
 
   return (
     <div>
       <BoardForm />
-
-      <BoardsList boards={boards}/>
+      <BoardsList boards={boards} />
     </div>
   );
 }
