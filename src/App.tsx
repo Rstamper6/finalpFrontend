34949f24c7d5b03e9b0f6c8b7184a75cc570ed0b
ZacharyNWilliams <zacharynwilliams9@gmail.com>
2023-03-16 18:04:08 -0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import UploadWidget from "./components/UploadWidget";
 import "./App.css";
+// import UploadWidget from "./components/UploadWidget";
+import "./App.css";
+import "../src/css/Services.css"
+import "../src/css/Board.css"
+import Services from "./components/Services";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
-      <UploadWidget />
+      {/* <UploadWidget /> */}
+      <Services/>
+      <Board/>
     </div>
   );
 }
