48a1a714d02d3d5def4663a5347cf36244bda517
Rstamper6 <rileystamper2@gmail.com>
2023-03-20 22:20:31 -0400

    signin start

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,11 @@
 import "../css/header.css";
 import { Link } from "react-router-dom";
+import { signInWithGoogle, signOut } from '../firebaseconfig';
+import AuthContext from '../context/AuthContext';
+import { useContext } from 'react';
 
 export function Header() {
+  const { user } = useContext(AuthContext)
   return (
     <div className="header">
       <div>
@@ -16,10 +20,16 @@ export function Header() {
       </div>
       <div className="login-signup-buttons">
         <div>
-          <button>Login</button>
-        </div>
-        <div>
-          <button>Sign up</button>
+          {
+            user ? 
+            <div>
+              <p>welcome {user.displayName}</p>
+              <button onClick={signOut}>Sign Out</button>
+            </div>  :
+            <div>
+              <button onClick={signInWithGoogle}>Sign in With Google</button>
+            </div>
+          }
         </div>
       </div>
     </div>
